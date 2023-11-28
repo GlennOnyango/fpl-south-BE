2bf37e56be30c6d772aa4f075f3d97561b8d6e4a
GlennOnyango <glenntedd@gmail.com>
2023-11-28 14:03:58 +0300

    rank cleaned

--- src/routes/rankRoute.ts
+++ src/routes/rankRoute.ts
@@ -1,9 +1,46 @@
 import express from "express";
+
+import jwt from "jsonwebtoken";
+//Controllers
+import { header } from "express-validator";
+
 const router = express.Router();
 
 import { getMonthlyRank, getWeeklyRank } from "../controllers/rankController";
 
-router.get("/weekly", getWeeklyRank);
-router.get("/monthly", getMonthlyRank);
+router.get(
+  "/weekly",
+  header("Authorization")
+    .notEmpty()
+    .withMessage("Authorization header is required"),
+  header("Authorization").custom((value: any, { req }: any) => {
+    const token = value.split(" ")[1];
+    try {
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+      req.userId = decoded.userId;
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }),
+  getWeeklyRank
+);
+router.get(
+  "/monthly",
+  header("Authorization")
+    .notEmpty()
+    .withMessage("Authorization header is required"),
+  header("Authorization").custom((value: any, { req }: any) => {
+    const token = value.split(" ")[1];
+    try {
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+      req.userId = decoded.userId;
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }),
+  getMonthlyRank
+);
 
 export default router;
